1951d0fc4ce6c929be7180ccf997715b371f4808
Sahana_Laks <sahana.l@1cloudhub.com>
2025-12-23 14:55:01 +0530

    quotes in export variables

--- cdk_code/cdk_code_stack.py
+++ cdk_code/cdk_code_stack.py
@@ -861,8 +861,8 @@ class CdkCodeStack(Stack):
         ec2_instance.add_user_data(  
             'set -e',          
             "sudo apt update -y",
-            f"export AWS_REGION={region}",
-            f"export S3_BUCKET={s3_bucket_name}",
+            f'export AWS_REGION="{region}"',
+            f'export S3_BUCKET="{s3_bucket_name}"',
             f'export REST_API_NAME="sap_rest_api-{unique_key}"',
             f'export WEBSOCKET_API_NAME="SAP_ws_{unique_key}"',
             "sudo apt install -y apache2 awscli jq postgresql-client-14",
